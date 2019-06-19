9bdfe1e7735b23fe864b892babfc8a8964cdbf8a
pmk0625 <pmk0625@hotmail.com>
2019-06-18 18:59:23 -0700

    Signed-off-by: pmk0625 <pmk0625@hotmail.com>

---
+++ Homework/Assignment 1/Hello World/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Minkyu R. Park
+ * Created on April 19, 2018, 11:30 AM
+ * Purpose:  Create a CSC/CIS 5 Template
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants - Math/Physics Constants, Conversions,
+//                   2-D Array Dimensions
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare Variables
+    cout<<"This program will display 'Hello World'"<<endl; //Explain the program
+    
+    //Initialize Variables
+    
+    //Process/Map inputs to outputs
+    
+    //Output data
+    cout<<"Hello World"<<endl; //Display
+    
+    //Exit stage right!
+    return 0;
+}
