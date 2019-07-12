968bf854cf89b9d88424f431e3aa1bc5157aa310
pmk0625 <pmk0625@hotmail.com>
2019-07-12 05:49:20 +0400

    Signed-off-by: pmk0625 <pmk0625@hotmail.com>

--- Book/MidTerm/Mid_Term_Problem_1/main.cpp
+++ Book/MidTerm/Mid_Term_Problem_1/main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 //Function Prototype
-void menu();
+char menu(char );
 
 //Constant Variable
 const int MAX_NUM = 20;
@@ -18,47 +18,61 @@ struct BankAccount
 
 int main(){
     //your code here
-    BankAccount account[MAX_NUM];
+    BankAccount account;
     bool complete = true;
-    
-    
     int choice;
-    while (choice < 1 || choice > 4 || complete){
-        complete = false;
-        menu();
-        cin>>choice;
+    
+    cout<<"Please enter your name: "<<endl;
+    getline (cin, account.name);
+    cout<<"Please enter the address: "<<endl;
+    getline (cin, account.address);
+    cout<<"Please enter the accNum: "<<endl;
+    getline (cin, account.accNum);
+    
+    
+    
+    choice = menu(choice);
+    if (choice >= '1' && choice <='4'){
         switch (choice){
             case '1':
             {
+                complete =  false;
                 cout<<"Current Balance Inquiry"<<endl;
                 break;
             }
             case '2':
             {
+                complete = false;
                 cout<<"Write Checks"<<endl;
                 break;
             }
             case '3':
             {
+                complete = false;
                 cout<<"Deposit Checks"<<endl;
                 break;
             }
             case '4':
             {
+                complete = false; 
                 cout<<"Cancel Transaction"<<endl;
-                complete = true;
+                break;
             }
+            
         }
     }
     
     return 0;
 }
 
-void menu(){
-    cout<<"Please enter from the following choices"<<endl;
-    cout<<"1. See current Balance"<<endl;
-    cout<<"2. Write Check"<<endl;
-    cout<<"3. Deposit Check"<<endl;
-    cout<<"4. Cancel Transaction"<<endl;
-    return;
+char menu(char choice){
+    do{
+        cout<<"Please choose from the following Menu"<<endl;
+        cout<<"1. Current Balance Inquiry"<<endl;
+        cout<<"2. Deposit Checks"<<endl;
+        cout<<"3. Withdraw Checks"<<endl;
+        cout<<"4. Exit Program"<<endl;
+        cin>>choice;
+    }while (choice == 1 || choice == 2 || choice == 3 || choice == 4);
+    return choice;
 }
